7dd646f484a6bb4603c6cef35770d5c2f5cca6d9
Stephen Onnen <stephen.onnen@gmail.com>
2025-03-14 18:50:38 -0500

    test schema

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,10 +1,24 @@
+import pytest
+from pydantic import ValidationError
+
 from book_api.schemas import BookSchema
 
 
-def create_book_with_invalid_isbn_should_raise_validation_error(book_service):
+def test_create_book_with_isbn_not_13_characters_should_raise_validation_error():
     book_data = {
         "title": "Test Book",
         "author": "Test Author",
         "isbn": "invalid_isbn",
     }
-    BookSchema.model_validate(**book_data)
+    with pytest.raises(ValidationError):
+        BookSchema.model_validate(book_data)
+
+
+def test_create_book_with_isbn_not_digit_should_raise_validation_error():
+    book_data = {
+        "title": "Test Book",
+        "author": "Test Author",
+        "isbn": "1234567890abc",
+    }
+    with pytest.raises(ValidationError):
+        BookSchema.model_validate(book_data)
